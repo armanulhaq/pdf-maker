ca647170641050e5aa9f8682ae701ccf6ec00a2a
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-10-01 12:32:12 +0530

    add margins

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ df = pd.read_csv('topics.csv')
 
 for index, row in df.iterrows():
     pdf.add_page()
+
     pdf.set_font(family="Times", style="B",size=24)
     pdf.set_text_color(100,100,100)
-    pdf.cell(w=0, h=14, txt=row['Topic'], align='L', ln=1)
-    pdf.line(10, 21, 200, 21)
+    pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
+    for y in range(20, 298, 10):
+        pdf.line(10, y, 200, y)
 
-    pdf.ln(260)
+    pdf.ln(265)
     #Set footer for master page
     pdf.set_font(family='Times', size=8)
     pdf.set_text_color(100, 100, 100)
@@ -28,5 +30,7 @@ for index, row in df.iterrows():
         pdf.set_font(family='Times', size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+        for y in range(20, 298, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output('output.pdf')
