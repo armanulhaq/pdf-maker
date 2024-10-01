41da1a27e76bbab623342def48bc92ff9b9cca93
Arman Ul Haq <72503393+armanulhaq@users.noreply.github.com>
2024-10-01 11:58:45 +0530

    add more pages for each topic

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ for index, row in df.iterrows():
     pdf.line(10, 21, 200, 21)
     #x1, y1, x2, y2 are the distance of starting and finishhing
     #points of line from top and sides
-
+    for i in range(row['Pages']):
+        pdf.add_page()
 
 pdf.output('output.pdf')
